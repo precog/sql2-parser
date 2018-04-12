3edd5e22b50f7e499856919683c67b43fb03b661
Daniel Spiewak <daniel@slamdata.com>
2018-04-12 10:52:04 -0600

    Cleaned up some more SBT things

--- project/CachedCi.scala
+++
@@ -1,9 +0,0 @@
-import org.romanowski.hoarder.actions.CachedCI
-import org.romanowski.hoarder.actions.ci.TravisPRValidation
-
-object CustomTravisValidation extends TravisPRValidation() {
-  override def shouldUseCache(): Boolean =
-    super.shouldUseCache() || sys.env.get("TRAVIS_BUILD_STAGE_NAME").map(_ != "Compile").getOrElse(false)
-}
-
-object CachedCi extends CachedCI.PluginBase(CustomTravisValidation)
